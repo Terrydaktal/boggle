5c2b77a356a17fb5ee0c68d3446ba399ea2ff1f6
Terrydaktal <9lewis9@gmail.com>
2024-01-02 22:40:04 +0000

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -2,29 +2,79 @@
 #include <netinet/in.h> // For sockaddr_in
 #include <cstdlib> // For exit() and EXIT_FAILURE
 #include <iostream> // For cout
+#include <sstream>
+#include <vector>
 #include <unistd.h> // For read
+#include <mysql.h>
+
+using namespace std;
+
+const char *dbserver = "localhost";
+const char *dbusername = "lewis";
+const char *dbpassword = "password";
+const char *dbdatabase = "wordelites";
+MYSQL mysql, *conn;
+
+
+int signup(vector<string> v) {
+	MYSQL_RES *res;
+	conn = mysql_real_connect(&mysql, dbserver, dbusername, dbpassword, dbdatabase, 0, 0, 0);
+	if (conn == NULL)
+	{
+		cout << mysql_error(&mysql) << endl << endl;
+		return 1;
+	}
+
+	string username = v[1];
+	string password = v[2];
+
+	std::string username;
+	std::string query = "SELECT * FROM users WHERE username =" + username + password;
+	query_state = mysql_query(conn, query);
+	if (query_state != 0)
+	{
+		cout << mysql_error(conn) << endl << endl;
+		return 1;
+	}
+	res = mysql_store_result(conn);
+
+	while ((row = mysql_fetch_row(res)) != NULL)
+	{
+		cout << left;
+		cout << setw(18) << row[0]
+			<< setw(18) << row[1]
+			<< setw(18) << row[2]
+			<< setw(18) << row[3] << endl;
+	}
+
+	mysql_free_result(res);
+	mysql_close(conn);
+}
 
 int main() {
-  // Create a socket (IPv4, TCP)
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1) {
-    std::cout << "Failed to create socket. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
-  }
 
-  // Listen to port 9999 on any address
-  sockaddr_in sockaddr;
-  sockaddr.sin_family = AF_INET;
-  sockaddr.sin_addr.s_addr = INADDR_ANY;
-  sockaddr.sin_port = htons(80); // htons is necessary to convert a number to
-                                   // network byte order
+	mysql_init(&mysql);
 
-  
-    //listen to all destination addresses on the incoming packet and on port 9999
-   //it sets _our_ address , and all packets destined to to it will be accepted
-	//and all packets sent from us will have this address and port
-	//effectively it means that it accepts all communications to port 9999 regardless of address
-	//and sends all packets from the appropriate outgoing address and port 9999
+	// Create a socket (IPv4, TCP)
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		std::cout << "Failed to create socket. errno: " << errno << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// Listen to port 9999 on any address
+	sockaddr_in sockaddr;
+	sockaddr.sin_family = AF_INET;
+	sockaddr.sin_addr.s_addr = INADDR_ANY;
+	sockaddr.sin_port = htons(80); // htons is necessary to convert a number to
+									 // network byte order
+
+
+	  //listen to all destination addresses on the incoming packet and on port 9999
+	 //it sets _our_ address , and all packets destined to to it will be accepted
+	  //and all packets sent from us will have this address and port
+	  //effectively it means that it accepts all communications to port 9999 regardless of address
+	  //and sends all packets from the appropriate outgoing address and port 9999
 
 	if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) {
 		std::cout << "Failed to bind to port 9999. errno: " << errno << std::endl;
@@ -32,31 +82,51 @@ int main() {
 	}
 
 
-  std::cout << "listening " << errno << std::endl;
-  // Start listening. Hold at most 10 connections in the queue
-  if (listen(sockfd, 10) < 0) {
-    std::cout << "Failed to listen on socket. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
-  }
-  std::cout << "done " << errno << std::endl;
-  // Grab a connection from the queue
-  auto addrlen = sizeof(sockaddr);
-  int connection = 0;
-  while ((connection = accept(sockfd, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen)) >= 0){
-	  if (connection < 0) {
-	    std::cout << "Failed to grab connection. errno: " << errno << std::endl;
-	    exit(EXIT_FAILURE);
-	  }
-	
-	  // Read from the connection
-	  char buffer[100];
-	  auto bytesRead = read(connection, buffer, 100);
-	  std::cout << "The message was: " << buffer;
-	  std::string response = "Good talking to you\n";
-	  send(connection, response.c_str(), response.size(), 0);
-  }
+	std::cout << "listening " << errno << std::endl;
+	// Start listening. Hold at most 10 connections in the queue
+	if (listen(sockfd, 10) < 0) {
+		std::cout << "Failed to listen on socket. errno: " << errno << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	std::cout << "done " << errno << std::endl;
+	// Grab a connection from the queue
+	auto addrlen = sizeof(sockaddr);
+	int connection = 0;
+	while ((connection = accept(sockfd, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen)) >= 0) {
+		if (connection < 0) {
+			std::cout << "Failed to grab connection. errno: " << errno << std::endl;
+			exit(EXIT_FAILURE);
+		}
 
-  // Close the connections
-  close(connection);
-  close(sockfd);
+		// Read from the connection
+		char buffer[100];
+		auto bytesRead = read(connection, buffer, 100);
+		
+		std::cout << "The message was: " << buffer;
+
+		string str(buffer);
+		vector<string> v;
+
+		stringstream ss(str);
+
+		while (ss.good()) {
+			string substr;
+			getline(ss, substr, ',');
+			v.push_back(substr);
+		}
+
+		for (size_t i = 0; i < v.size(); i++)
+			cout << v[i] << endl;
+
+		if (memcmp(buffer, "signup", 6)) {
+			signup(v);
+		}
+
+		std::string response = "Good talking to you\n";
+		send(connection, response.c_str(), response.size(), 0);
+	}
+
+	// Close the connections
+	close(connection);
+	close(sockfd);
 }
